96bf278a4623a59d1f662a0cf3a06af660721d19
Muhammet <33325253+blackdragonx61@users.noreply.github.com>
2019-10-20 12:36:17 +0300

    Deleted std::filesystem

--- 1.Svn/Client/EterBase/Debug.cpp
+++ 1.Svn/Client/EterBase/Debug.cpp
@@ -4,17 +4,14 @@ HWND g_PopupHwnd = NULL;
 ///Add
 #if defined(CUSTOM_LOGS)
 #include <memory>
-#include <filesystem>
 class CustomLogFile : public CSingleton<CustomLogFile>
 {
 	public:
-		CustomLogFile() { 
+		CustomLogFile(const std::string& path) { 
 			m_fp.clear();
-			const std::string path = "logs/";
-			if (!std::filesystem::is_directory(path) || !std::filesystem::exists(path))
-				std::filesystem::create_directory(path);
-			for (const auto& v : { "ITEM_ERROR.txt", "MAP_ERROR.txt" })
-				m_fp.emplace_back(std::make_unique<FILE*>(fopen(std::string(path + v).c_str(), "w")));
+			CreateDirectory(path.c_str(),nullptr);
+			for (const std::string& v : { "ITEM_ERROR.txt", "MAP_ERROR.txt" })
+				m_fp.emplace_back(std::make_unique<FILE*>(fopen((path + "/" + v).c_str(), "w")));
 		}
 		virtual ~CustomLogFile() {
 			for (auto& v : m_fp)
@@ -24,15 +21,15 @@ class CustomLogFile : public CSingleton<CustomLogFile>
 		void Write(const char* c_pszMsg, BYTE Type) {
 			if (Type >= m_fp.size() || !*m_fp[Type])
 				return;
-			const __time64_t ct = time(nullptr);
-			const tm ctm = *localtime(&ct);
+			const auto ct = time(nullptr);
+			const auto ctm = *localtime(&ct);
 			fprintf(*m_fp[Type], "%02d%02d %02d:%02d:%05d :: %s\n",
 				ctm.tm_mon + 1, ctm.tm_mday, ctm.tm_hour, ctm.tm_min,
 				ELTimer_GetMSec() % 60000, c_pszMsg);
 			fflush(*m_fp[Type]);
 		}
 	protected:
-		std::vector < std::unique_ptr<FILE*> > m_fp;
+		std::vector <std::unique_ptr<FILE*>> m_fp;
 };
 #endif
 
@@ -41,7 +38,7 @@ static CLogFile gs_logfile;
 
 ///Add
 #if defined(CUSTOM_LOGS)
-static CustomLogFile JustForInit;
+static CustomLogFile JustForInit("logs");
 void LogFileWithType(BYTE Type, const char * c_szMessage, ...)
 {
 	va_list args;
