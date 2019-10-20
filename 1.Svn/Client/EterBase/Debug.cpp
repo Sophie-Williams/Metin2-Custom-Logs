38826698c582e3b5a4dc85ba6a36b4c597154f2d
Muhammet <33325253+blackdragonx61@users.noreply.github.com>
2019-10-20 16:57:25 +0300

    Update Debug.cpp

--- 1.Svn/Client/EterBase/Debug.cpp
+++ 1.Svn/Client/EterBase/Debug.cpp
@@ -8,28 +8,23 @@ class CustomLogFile : public CSingleton<CustomLogFile>
 {
 	public:
 		CustomLogFile(const std::string& path) { 
-			m_fp.clear();
-			CreateDirectory(path.c_str(),nullptr);
-			for (const std::string& v : { "ITEM_ERROR.txt", "MAP_ERROR.txt" })
-				m_fp.emplace_back(std::make_unique<FILE*>(fopen((path + "/" + v).c_str(), "w")));
+			if (CreateDirectory(path.c_str(), nullptr))
+				for (const std::string& v : { "ITEM_ERROR.txt", "MAP_ERROR.txt" })
+					m_fp.emplace_back(unique_ptr<std::FILE, decltype(&fclose)>(fopen((path + "/" + v).c_str(), "a"), &fclose));		
 		}
-		virtual ~CustomLogFile() {
-			for (auto& v : m_fp)
-				fclose(*v);
-			m_fp.clear();
-		}
+		virtual ~CustomLogFile() {m_fp.clear();}
 		void Write(const char* c_pszMsg, BYTE Type) {
-			if (Type >= m_fp.size() || !*m_fp[Type])
+			if (Type >= m_fp.size() || !m_fp.at(Type))
 				return;
 			const auto ct = time(nullptr);
 			const auto ctm = *localtime(&ct);
-			fprintf(*m_fp[Type], "%02d%02d %02d:%02d:%05d :: %s\n",
+			fprintf(m_fp.at(Type).get(), "%02d%02d %02d:%02d:%05d :: %s\n",
 				ctm.tm_mon + 1, ctm.tm_mday, ctm.tm_hour, ctm.tm_min,
 				ELTimer_GetMSec() % 60000, c_pszMsg);
-			fflush(*m_fp[Type]);
+			fflush(m_fp.at(Type).get());
 		}
 	protected:
-		std::vector <std::unique_ptr<FILE*>> m_fp;
+		std::vector <std::unique_ptr<std::FILE, decltype(&fclose)>> m_fp;
 };
 #endif
 
@@ -45,6 +40,7 @@ void LogFileWithType(BYTE Type, const char * c_szMessage, ...)
 	va_start(args, c_szMessage);
 	char szBuf[DEBUG_STRING_MAX_LEN+1];
 	_vsnprintf(szBuf, sizeof(szBuf), c_szMessage, args);
+	va_end(args);
 	CustomLogFile::Instance().Write(szBuf, Type);
 }
 #endif
